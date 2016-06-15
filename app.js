7a6e899da58ea8b0830ef068f5e2dd9ba2f65b32
Maksim Daunarovich <max.daunarovich@gmail.com>
2016-06-15 13:37:17 +0200

    Implement basic koa routing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const
     APORT = 3320,
+    Router = require('koa-router'),
     koa = require('koa'),
     app = koa();
 
@@ -10,11 +11,26 @@ function *reqlogger(next){
     console.log('%s - %s %s', new Date().toISOString(), this.req.method, this.req.url);
     yield next;
 }
-
 app.use(reqlogger);
 
+app.use(Router(app));
+
+app.get('/', function *(){
+    console.log("Express-style example");
+    this.body = "This is root page";
+});
+
+const publicRouter = new Router();
+
+publicRouter.get('/auth/github', function *(){
+    console.log("Middleware-style example");
+    this.body = "Authenticate with GitHub OAUTH API (Coming soon)";
+});
+
+app.use(publicRouter.middleware());
+
 app.use(function *(){
-   this.body = "Hello, world!"
+    this.body = "Hello, world!";
 });
 
 app.listen(APORT);
